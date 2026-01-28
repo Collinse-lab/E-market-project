84444009b835d9084ae26b13268e3f0a61d28eab
Collinse <rowanepeters@gmail.com>
2026-01-28 15:44:22 +0300

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,31 @@ import Logo from './assets/image-1.jpg'
 import './styles.css'
 
 
+
+const products = [
+  {id:1,text:"Apple"},
+  {id:2,text:"Oranges"},
+  {id:3,text:"Mangoes"},
+  {id:4,text:"Pears"},
+  {id:5,text:"Lemon"},
+];
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='line'>
         <a href="https://e-market.com" target="_blank">
           <img src={Logo} className="logo" alt="Market logo" />
         </a>
+        <h1>E-Market</h1>
+        
+  <input
+          type="text"
+          placeholder="Search items..."
+        />
+       
       </div>
-      <h1>E-Market</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
